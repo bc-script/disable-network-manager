56ccd382974e294b7956d53791821453d6bcf49b
Prince Nyarko <nyankonton122@gmail.com>
2023-04-26 11:33:41 -0300

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 
 ## apt install iptables
+iptables -F
+
 # DNS 
 iptables -A INPUT -p tcp --dport 80 -s 10.7.5.0/24 -j ACCEPT
 iptables -A INPUT -p tcp --dport 80 -s 10.7.7.0/24 -j ACCEPT
@@ -9,9 +11,12 @@ iptables -A INPUT -p tcp --dport 80 -s 10.7.7.0/24 -j ACCEPT
 iptables -A INPUT -p tcp --dport 7283 -s 10.7.5.0/24 -j ACCEPT
 iptables -A INPUT -p tcp --dport 7283 -s 200.239.87.0/24 -j ACCEPT
 
+# Barra as portas
 iptables -A INPUT -p tcp --dport 80 -j DROP
 iptables -A INPUT -p tcp --dport 7283 -j DROP
+iptables -A INPUT -p tcp --dport 22 -j DROP
 
+# Barra os SCANNERS de portas
 iptables -N SCANNER
 iptables -A SCANNER -m limit --limit 3/m -j LOG --log-level 6 --log-prefix "FW: port scanner: "
 iptables -A SCANNER -m recent --name blacklist_60 --set -m comment --comment "Drop/Blacklist Null scan" -j DROP
@@ -23,7 +28,6 @@ iptables -A SCANNER -m limit --limit 3/m --limit-burst 5 -j LOG --log-prefix "Fi
 iptables -A SCANNER -m comment --comment "Drop/Blacklist Xmas/PSH scan" -j DROP # Xmas-PSH scan
 iptables -A SCANNER -m comment --comment "Drop/Blacklist Xmas scan" -j DROP # Against nmap -sX (Xmas tree scan)
 iptables -A SCANNER -m comment --comment "Drop/Blacklist Xmas/All scan" -j DROP # Xmas All scan
-
 
 iptables -A SCANNER -j DROP
 
